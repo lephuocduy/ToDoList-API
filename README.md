afe9af98e2ee786eb7683f9fe0b9e952e7c0d3db
lephuocduy <duylp.work@gmail.com>
2021-06-29 00:53:06 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Welcome to ToDoList-API repository.
+
+#### Connect me via:
+[My Facebook](https://facebook.com/duydeptraiqua)  
+[My Email](mailto: duylp.work@gmail.com)
+
+© 2021 lephuocduy
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapToDoList from './BaiTapToDoList/BaiTapToDoList';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapToDoList />
+    </div>
+  );
+}
+
+export default App;
